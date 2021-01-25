b4fc77f5eb7ab4b332ed071665de287350349920
Yu <langyu@88e9fe7cabd7.ant.amazon.com>
2021-01-25 23:43:21 +0800

    add basic static auth

--- POC-s3_presign_ipv6/src/main/java/s3_presign_ipv6/App.java
+++ POC-s3_presign_ipv6/src/main/java/s3_presign_ipv6/App.java
@@ -3,12 +3,13 @@ package s3_presign_ipv6;
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.HttpMethod;
 import com.amazonaws.SdkClientException;
-import com.amazonaws.auth.InstanceProfileCredentialsProvider;
+//import com.amazonaws.auth.InstanceProfileCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
-import com.amazonaws.services.s3.model.S3Object;
 
 import java.io.IOException;
 import java.net.URL;
@@ -17,15 +18,17 @@ public class App {
 
     public static void main(String[] args) throws IOException {
         Regions clientRegion = Regions.CN_NORTHWEST_1;
-        String bucketName = "Your_Bucket_Name";
-        String objectKey = "Your_Object_Path";
+        String bucketName = "billing-cur-langyu";
+        String objectKey = "curtest/CUR/20210101-20210201/CUR-Manifest.json";
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials("Your-AK", "Your-SK");
 
         try {
             AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
-                    .withCredentials(new InstanceProfileCredentialsProvider(false))
-                    .withRegion(clientRegion)
-                    .withDualstackEnabled(true)
-                    .build();
+                //.withCredentials(new InstanceProfileCredentialsProvider(false))
+                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+                .withRegion(clientRegion)
+                .withDualstackEnabled(true)
+                .build();
 
             // Set the pre-signed URL to expire after one hour.
             java.util.Date expiration = new java.util.Date();
@@ -36,19 +39,19 @@ public class App {
             // Generate the pre-signed URL.
             System.out.println("Generating pre-signed URL.");
             GeneratePresignedUrlRequest generatePresignedUrlRequest = new GeneratePresignedUrlRequest(bucketName, objectKey)
-                    .withMethod(HttpMethod.GET)
-                    .withExpiration(expiration);
+                .withMethod(HttpMethod.GET)
+                .withExpiration(expiration);
             URL url = s3Client.generatePresignedUrl(generatePresignedUrlRequest);
-	    System.out.println(url.toString());
+            System.out.println(url.toString());
 
-	    System.exit(0);
+            System.exit(0);
 
         } catch (AmazonServiceException e) {
-            // The call was transmitted successfully, but Amazon S3 couldn't process 
+            // The call was transmitted successfully, but Amazon S3 couldn't process
             // it, so it returned an error response.
             e.printStackTrace();
         } catch (SdkClientException e) {
-            // Amazon S3 couldn't be contacted for a response, or the client  
+            // Amazon S3 couldn't be contacted for a response, or the client
             // couldn't parse the response from Amazon S3.
             e.printStackTrace();
         }
